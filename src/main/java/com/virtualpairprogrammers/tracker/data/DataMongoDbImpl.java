e16f9149effc2777420cbed18cfe53a7c9679143
Stanislav Khurtesov <s.khurtesov@gmail.com>
2022-06-17 03:42:56 +0400

    Avoiding .withVehiclePostion error

--- src/main/java/com/virtualpairprogrammers/tracker/data/DataMongoDbImpl.java
+++ src/main/java/com/virtualpairprogrammers/tracker/data/DataMongoDbImpl.java
@@ -37,7 +37,7 @@ public class DataMongoDbImpl implements Data {
 	public void updatePosition(VehiclePosition position) {
 		String vehicleName = position.getName();
 		BigDecimal speed = calculateSpeedInMph(vehicleName, position);
-		VehiclePosition vehicleWithSpeed = new VehicleBuilder().withVehiclePostion(position).withSpeed(new BigDecimal("47.5")).build();
+		VehiclePosition vehicleWithSpeed = new VehicleBuilder().withVehiclePostion(position).withSpeed(speed).build();
 		mongoDb.insert(vehicleWithSpeed);
 	}
 
